2fadc69fa363b1b43f25548bf1aa986cd371d181
walgab <gabriel.diem5@gmail.com>
2023-03-24 11:56:51 -0300

    Made first build

---
+++ dist/404.df416b65.js
@@ -0,0 +1,2 @@
+var o="localhost"!==window.location.hostname&&!window.location.href.includes("127.0.0.1")?"/maulhardt-save":"";window.location.replace(`${o}/`);
+//# sourceMappingURL=404.df416b65.js.map
