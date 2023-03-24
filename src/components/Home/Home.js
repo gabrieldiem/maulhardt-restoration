1b180888e456db2458a2cd96e65954b4056aee8d
walgab <gabriel.diem5@gmail.com>
2023-03-24 19:42:15 -0300

    Debug analisis2Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import analisis2Page from '../../pages/analisis-2.html';
 
 const Home = () => {
   return <div className="">
+    {console.log(analisis2Page)}
     <a href={`${analisis2Page}`}>adasdasdas</a><br/>
     <Link to="analisis-2">Analisis 2</Link><br/>
     <Link to="analisis-3">Analisis 3</Link><br/>
