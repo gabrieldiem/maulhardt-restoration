c53c7cc6836ecf98a6db6b2ce3ab0018aee9330b
walgab <gabriel.diem5@gmail.com>
2023-03-24 19:35:20 -0300

    Testing MPA

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,12 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 
+import baseRelativePath from '../../path';
+import analisis2Page from '../../pages/analisis-2.html';
+
 const Home = () => {
   return <div className="">
+    <a href={`${baseRelativePath}${analisis2Page}`}>adasdasdas</a><br/>
     <Link to="analisis-2">Analisis 2</Link><br/>
     <Link to="analisis-3">Analisis 3</Link><br/>
     <Link to="matematica-discreta">Matematica Discreta</Link><br/>
