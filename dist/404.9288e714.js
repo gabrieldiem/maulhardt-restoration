398f65d15080e840c1692fa4173ea68ae1e579aa
walgab <gabriel.diem5@gmail.com>
2023-03-24 11:59:48 -0300

    Fixed old .env incorrect location

---
+++ dist/404.9288e714.js
@@ -0,0 +1,2 @@
+var o="localhost"!==window.location.hostname&&!window.location.href.includes("127.0.0.1")?"/maulhardt-restoration":"";window.location.replace(`${o}/`);
+//# sourceMappingURL=404.9288e714.js.map
--- dist/404.df416b65.js
+++
@@ -1,2 +0,0 @@
-var o="localhost"!==window.location.hostname&&!window.location.href.includes("127.0.0.1")?"/maulhardt-save":"";window.location.replace(`${o}/`);
-//# sourceMappingURL=404.df416b65.js.map
