c018d64958086607048353e3f63a8b96b5e2018b
walgab <gabriel.diem5@gmail.com>
2023-03-25 20:15:44 -0300

    Trying stuff to fix routing

--- src/pages/analisis-2.js
+++ src/pages/analisis-2.js
@@ -12,6 +12,30 @@ import Analisis2 from '../components/Analisis2/Analisis2';
 import MaterialOficial from '../components/Analisis2/MaterialOficial/MaterialOficial';
 import Redir from '../components/Analisis2/Redir/Redir';
 
+console.log('IM IN AM2');
+(function () {
+  // Get the current location of the page
+  const currentLocation = window.location;
+  console.log(window.location);
+  console.log(window.location.search);
+
+  // Check if there is a redirect in the query string
+  if (currentLocation.search[1] === '/') {
+    // Decode the redirect in the query string and replace "~and~" with "&"
+    const decodedRedirect = currentLocation.search
+      .slice(1)
+      .split('&')
+      .map(function (str) {
+        console.log(str);
+        return str.replace(/~and~/g, '&');
+      })
+      .join('?');
+
+    // Replace the current state in the browser history with the decoded redirect
+    window.history.replaceState(null, null, currentLocation.pathname.slice(0, -1) + decodedRedirect + currentLocation.hash);
+  }
+})();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
