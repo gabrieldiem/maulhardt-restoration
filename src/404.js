2a2641f5ab3057c2f9dd958b6a149c14b8fdcb94
walgab <gabriel.diem5@gmail.com>
2023-03-28 14:22:27 -0300

    Refactor: 404 routing logic code cleaned

--- src/404.js
+++ src/404.js
@@ -4,29 +4,16 @@ import pagesData from './data/shared-data';
 const currentURL = window.location.href;
 let currentPageState = null;
 
-// Single Page Apps for GitHub Pages
-// MIT License
-// https://github.com/rafgraph/spa-github-pages
-// This script takes the current url and converts the path and query
-// string into just a query string, and then redirects the browser
-// to the new url with only a query string and hash fragment,
-// e.g. https://www.foo.tld/one/two?a=b&c=d#qwe, becomes
-// https://www.foo.tld/?/one/two&a=b~and~c=d#qwe
-// Note: this 404.html file must be at least 512 bytes for it to work
-// with Internet Explorer (it is currently > 512 bytes)
+function getFullHostname () {
+  const currentLocation = window.location;
+  let hostname = currentLocation.protocol + '//' + currentLocation.hostname;
 
-// If you're creating a Project Pages site and NOT using a custom domain,
-// then set pathSegmentsToKeep to 1 (enterprise users may need to set it to > 1).
-// This way the code will only replace the route part of the path, and not
-// the real directory in which the app resides, for example:
-// https://username.github.io/repo-name/one/two?a=b&c=d#qwe becomes
-// https://username.github.io/repo-name/?/one/two&a=b~and~c=d#qwe
-// Otherwise, leave pathSegmentsToKeep as 0.
-// Define the number of path segments to keep
-// const pathSegmentsToKeep = (baseRelativePath === '') ? 0 : 1;
+  if (currentLocation.port && currentLocation.port !== '') {
+    hostname += ':' + currentLocation.port;
+  }
 
-// // Get the current location of the page
-// const currentLocation = window.location;
+  return hostname;
+}
 
 pagesData.forEach(page => {
   if (currentURL.includes(`${baseRelativePath}/pages/${page}`)) {
@@ -35,31 +22,7 @@ pagesData.forEach(page => {
 });
 
 if (currentPageState && currentPageState.isAPage) {
-  // Build the new URL
-  // let newUrl = currentLocation.protocol + '//';
-  // newUrl += currentLocation.hostname;
-  // if (currentLocation.port && currentLocation.port !== '') {
-  //   newUrl += ':' + currentLocation.port;
-  // }
-
-  // newUrl += '/' + currentLocation.pathname.split('/').slice(0, 1 + pathSegmentsToKeep)[pathSegmentsToKeep] + ((baseRelativePath === '') ? '' : '/') + `pages/${currentPageState.currentPage}.html` + '/?/';
-  // newUrl += currentLocation.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~').split(`${currentPageState.currentPage}/`)[1];
-  // console.log('NEW', newUrl);
-  // // Redirect the browser to the new URL
-  // currentLocation.replace(newUrl);
-
-  const fullHostname = (() => {
-    const currentLocation = window.location;
-
-    // Build the new URL
-    let newUrl = currentLocation.protocol + '//';
-    newUrl += currentLocation.hostname;
-    if (currentLocation.port && currentLocation.port !== '') {
-      newUrl += ':' + currentLocation.port;
-    }
-
-    return newUrl;
-  })();
+  const fullHostname = getFullHostname();
 
   let preciseEndpoint = window.location.href.split(`${fullHostname}${baseRelativePath}/pages/${currentPageState.currentPage}`)[1];
 
@@ -73,7 +36,6 @@ if (currentPageState && currentPageState.isAPage) {
 
   // document.cookie = 'sessionStorageSameSite=None;SameSite=None;Secure';
   sessionStorage.setItem('redirect', JSON.stringify({ fullUrl: window.location.href, page: currentPageState.currentPage, endpoint: preciseEndpoint, baseRelativePath }));
-  console.log('In 404', sessionStorage.getItem('redirect'));
   window.location.replace(`${baseRelativePath}/pages/${currentPageState.currentPage}.html`);
 } else {
   window.location.replace(`${baseRelativePath}/`);
--- src/index.js
+++ src/index.js
@@ -9,27 +9,6 @@ import Home from './components/Home/Home';
 import './index.css';
 import baseRelativePath from './path';
 
-console.log('IM IN INDEX.js');
-// (function () {
-//   // Get the current location of the page
-//   const currentLocation = window.location;
-
-//   // Check if there is a redirect in the query string
-//   if (currentLocation.search[1] === '/') {
-//     // Decode the redirect in the query string and replace "~and~" with "&"
-//     const decodedRedirect = currentLocation.search
-//       .slice(1)
-//       .split('&')
-//       .map(function (str) {
-//         return str.replace(/~and~/g, '&');
-//       })
-//       .join('?');
-
-//     // Replace the current state in the browser history with the decoded redirect
-//     window.history.replaceState(null, null, currentLocation.pathname.slice(0, -1) + decodedRedirect + currentLocation.hash);
-//   }
-// })();
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
