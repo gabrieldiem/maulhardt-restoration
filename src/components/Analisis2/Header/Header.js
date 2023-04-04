97e37b86b815002f20b8c0b07b07b6789a7c3e6c
walgab <gabriel.diem5@gmail.com>
2023-04-03 23:26:44 -0300

    Disabled 'Libro de visitas' header link

--- src/components/Analisis2/Header/Header.js
+++ src/components/Analisis2/Header/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
               </ul>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="#">LIBRO DE VISITAS</Link>
+              <Link className="nav-link disabled" aria-current="page" to="#" >LIBRO DE VISITAS</Link>
             </li>
           </ul>
         </div>
